c659b793b9dc74cbe03c9360eadd65c48ecc3da6
Malin Sundberg <143407879+Malinsund@users.noreply.github.com>
2025-03-13 21:13:48 +0100

    a little on postpage

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,10 +2,11 @@
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import PostCard from "@/components/PostCard";
+import PostModal from "@/components/PostModal";
 import { useProfile } from "@/context/ProfileContext";
 import { mockPosts } from "@/data/mockPosts";
 import { db } from "@/firebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
 interface Post {
@@ -20,10 +21,13 @@ interface Post {
 export default function PostPage() {
   const { imageUrl } = useProfile();
   const [posts, setPosts] = useState<Post[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const querySnapshot = await getDocs(collection(db, "posts"));
+      const querySnapshot = await getDocs(
+        query(collection(db, "posts"), orderBy("createdAt", "desc"))
+      );
       const postsData = querySnapshot.docs.map((doc) => doc.data());
 
       const posts = postsData.map((post: any) => ({
@@ -40,24 +44,43 @@ export default function PostPage() {
 
     fetchPosts();
   }, []);
+
+  const handlePostAdded = (newPost: Post) => {
+    setPosts((prevPosts) => [newPost, ...prevPosts]);
+  };
+
   return (
     <div>
       <Header />
-      <div className="hidden lg:block sticky top-">
+      <div className="hidden lg:block sticky top-0">
         <Navbar />
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-4 text-center gap-2">
         <div className="lg:col-span-1 bg-purple-300">
-          {" "}
-          <p>heeej lägg upp ett nytt inlägg hääär</p>
-          {/* lägg upp nytt inlägg */}
+          <div>
+            <button
+              onClick={() => setIsModalOpen(true)}
+              className="bg-blue-500 text-white p-2 rounded w-80"
+            >
+              Lägg upp nytt inlägg
+            </button>
+
+            <PostModal
+              isOpen={isModalOpen}
+              onClose={() => setIsModalOpen(false)}
+            />
+          </div>
         </div>
         <div className="lg:col-span-2 bg-yellow-200 overflow-y-scroll h-screen">
           <h1>
             Flöde - Här skall inlägged vara som stora kort (skapa komponent för
             korten)
           </h1>
-          {/* {posts.map((post) => ( */}
+
+          {/* 
+          {posts.map((post) => (
+            
+            */}
           {mockPosts.map((post) => (
             <PostCard
               key={post.id}
