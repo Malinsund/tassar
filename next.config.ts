88f24fd1083f2538d494b9ccafde080773f0b7fe
Malin Sundberg <143407879+Malinsund@users.noreply.github.com>
2025-04-03 15:43:49 +0200

    Try20

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-    images: {
-      domains: ['firebasestorage.googleapis.com', 'example.com'],
-    },
+  images: {
+    domains: ['firebasestorage.googleapis.com', 'example.com'],
+  },
+  webpack(config) {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
 };
 
 export default nextConfig;
