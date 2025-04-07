e5cfe435815ce3df4a7d66b712cef9e8c1cb183f
Malin Sundberg <143407879+Malinsund@users.noreply.github.com>
2025-04-07 20:42:05 +0200

    trying again

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -11,7 +11,7 @@ import { XCircleIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import router from "next/router";
-import { Suspense, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ProfilePage() {
   const { user } = useAuth();
@@ -67,111 +67,109 @@ export default function ProfilePage() {
   };
 
   return (
-    <Suspense fallback={<div>Laddar...</div>}>
-      <div>
-        <Header />
-        <div className="hidden lg:block">
-          <Navbar />
-        </div>
-        <div className="grid grid-cols-1 lg:grid-cols-4 lg:h-screen gap-2">
-          {/* Profilinformation */}
-          <div className="flex flex-col items-center col-span-1 p-4 gap-2 lg:gap-4">
-            <ProfileImage
-              userId={userIdToShow!}
-              isEditing={isEditing && isOwnProfile}
-              size={200}
-            />
-            {username && (
-              <h2 className="text-2xl font-bold self-start lg:self-center text-left lg:text-center">
-                @{username}
-              </h2>
-            )}
-            <div className="text-left lg:text-center">
-              {isEditing && isOwnProfile ? (
-                <textarea
-                  className="rounded-lg border-grey10 w-56 h-24 lg:w-72 p-2"
-                  value={newDescription}
-                  onChange={(e) => setNewDescription(e.target.value)}
-                />
-              ) : (
-                <p>{description || "Ingen beskrivning satt"}</p>
-              )}
-            </div>
-            {isOwnProfile && (
-              <div className="flex lg:mt-8 gap-8">
-                <PrimaryButton
-                  onClick={() => {
-                    if (isEditing) saveAllChanges();
-                    setIsEditing(!isEditing);
-                  }}
-                  text={isEditing ? "Spara" : "Redigera profil"}
-                  className="lg:place-content-end"
-                />
-              </div>
-            )}
-          </div>
-
-          {/* Bilder */}
-          <div className="border-y-2 lg:border-y-0 lg:border-x-2 p-2 col-span-2 lg:flex-grow h-full">
-            <h1>bilder</h1>
-            <div className="grid grid-cols-4 gap-2">
-              {userImages.map((image, index) => (
-                <div key={index} className="relative m-2">
-                  {isEditing && isOwnProfile && (
-                    <button
-                      className="absolute -top-2 -right-2"
-                      onClick={() => {
-                        setSelectedImage(image);
-                        setShowConfirm(true);
-                      }}
-                    >
-                      <XCircleIcon className="w-8 h-8 rounded-full fill-white" />
-                    </button>
-                  )}
-                  <Image
-                    src={image}
-                    alt={`User Image ${index}`}
-                    className="w-32 h-32 object-cover rounded-lg"
-                    width={100}
-                    height={100}
-                  />
-                </div>
-              ))}
-            </div>
-          </div>
-
-          {/* Meddelanden */}
-          <div className="col-span-1 lg:col-span-1 lg:block p-4">
-            <h2 className="text-xl font-bold">Meddelanden</h2>
-          </div>
-        </div>
-
-        {/* Bekräftelse för att ta bort bild */}
-        {showConfirm && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-            <div className="bg-white p-4 rounded shadow-lg text-center">
-              <p className="text-xl font-special">
-                Är du säker på att du vill ta bort denna bild?
-              </p>
-              <p className="font-poppins">Denna åtgärd går inte att ångra</p>
-              <div className="flex justify-center gap-4 mt-4">
-                <button
-                  onClick={handleDeleteImage}
-                  className="bg-destructive text-white font-special px-4 py-2 rounded"
-                >
-                  Ja, ta bort
-                </button>
-                <button
-                  onClick={() => setShowConfirm(false)}
-                  className="bg-grey50 text-white px-4 py-2 rounded"
-                >
-                  Avbryt
-                </button>
-              </div>
-            </div>
-          </div>
-        )}
+    <div>
+      <Header />
+      <div className="hidden lg:block">
+        <Navbar />
       </div>
-    </Suspense>
+      <div className="grid grid-cols-1 lg:grid-cols-4 lg:h-screen gap-2">
+        {/* Profilinformation */}
+        <div className="flex flex-col items-center col-span-1 p-4 gap-2 lg:gap-4">
+          <ProfileImage
+            userId={userIdToShow!}
+            isEditing={isEditing && isOwnProfile}
+            size={200}
+          />
+          {username && (
+            <h2 className="text-2xl font-bold self-start lg:self-center text-left lg:text-center">
+              @{username}
+            </h2>
+          )}
+          <div className="text-left lg:text-center">
+            {isEditing && isOwnProfile ? (
+              <textarea
+                className="rounded-lg border-grey10 w-56 h-24 lg:w-72 p-2"
+                value={newDescription}
+                onChange={(e) => setNewDescription(e.target.value)}
+              />
+            ) : (
+              <p>{description || "Ingen beskrivning satt"}</p>
+            )}
+          </div>
+          {isOwnProfile && (
+            <div className="flex lg:mt-8 gap-8">
+              <PrimaryButton
+                onClick={() => {
+                  if (isEditing) saveAllChanges();
+                  setIsEditing(!isEditing);
+                }}
+                text={isEditing ? "Spara" : "Redigera profil"}
+                className="lg:place-content-end"
+              />
+            </div>
+          )}
+        </div>
+
+        {/* Bilder */}
+        <div className="border-y-2 lg:border-y-0 lg:border-x-2 p-2 col-span-2 lg:flex-grow h-full">
+          <h1>bilder</h1>
+          <div className="grid grid-cols-4 gap-2">
+            {userImages.map((image, index) => (
+              <div key={index} className="relative m-2">
+                {isEditing && isOwnProfile && (
+                  <button
+                    className="absolute -top-2 -right-2"
+                    onClick={() => {
+                      setSelectedImage(image);
+                      setShowConfirm(true);
+                    }}
+                  >
+                    <XCircleIcon className="w-8 h-8 rounded-full fill-white" />
+                  </button>
+                )}
+                <Image
+                  src={image}
+                  alt={`User Image ${index}`}
+                  className="w-32 h-32 object-cover rounded-lg"
+                  width={100}
+                  height={100}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Meddelanden */}
+        <div className="col-span-1 lg:col-span-1 lg:block p-4">
+          <h2 className="text-xl font-bold">Meddelanden</h2>
+        </div>
+      </div>
+
+      {/* Bekräftelse för att ta bort bild */}
+      {showConfirm && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+          <div className="bg-white p-4 rounded shadow-lg text-center">
+            <p className="text-xl font-special">
+              Är du säker på att du vill ta bort denna bild?
+            </p>
+            <p className="font-poppins">Denna åtgärd går inte att ångra</p>
+            <div className="flex justify-center gap-4 mt-4">
+              <button
+                onClick={handleDeleteImage}
+                className="bg-destructive text-white font-special px-4 py-2 rounded"
+              >
+                Ja, ta bort
+              </button>
+              <button
+                onClick={() => setShowConfirm(false)}
+                className="bg-grey50 text-white px-4 py-2 rounded"
+              >
+                Avbryt
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
