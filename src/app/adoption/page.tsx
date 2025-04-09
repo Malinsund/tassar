e938f72fc32fae38202b298dd25f8c0fae836d49
Malin Sundberg <143407879+Malinsund@users.noreply.github.com>
2025-04-09 15:31:25 +0200

    UI fixes in adoptin

--- src/app/adoption/page.tsx
+++ src/app/adoption/page.tsx
@@ -1,7 +1,10 @@
+import Header from "@/components/Header";
+
 export default function Adoption() {
   return (
-    <main className="w-full h-full p-36">
-      <div className="flex flex-col justify-center text-center">
+    <main className="w-full h-full ">
+      <Header />
+      <div className="flex flex-col justify-center text-center py-20 lg:p-36">
         <h1 className="text-4xl font-special font bold">
           Innehåll kommer snart!
         </h1>
